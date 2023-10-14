266ef24ec559b8feff8769bc4727e025d1a0429f
RazvanV12 <92295676+RazvanV12@users.noreply.github.com>
2023-10-14 23:56:28 +0300

    .

---
+++ CubeGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static int currentScore;
+    public static int highScore;
+
+    public static int currentLevel = 0;
+    public static int unlockedLevel;
+
+    public static void CompleteLevel()
+    {
+        currentLevel += 1;
+        SceneManager.LoadScene(currentLevel);
+    }
+}
---
+++ CubeGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private float maxSpeed = 5f;
+    public GameObject deathParticles;
+
+    private Vector3 input;
+
+    private Vector3 spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.position += input * moveSpeed * Time.deltaTime;
+        
+        if(transform.position.y < -2)
+            Die();
+        
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Die();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            GameManager.CompleteLevel();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathParticles, transform.position, Quaternion.identity);
+        transform.position = spawn;
+    }
+}
